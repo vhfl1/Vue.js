72834c3a985b0379fd9384b54a76359497b346f0
vhfl1 <111475086+vhfl1@users.noreply.github.com>
2023-04-10 11:16:19 +0900

    2023-04-10

---
+++ Ch05/src/components/sub2/Child1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4>Child1</h4>
+  </div>
+</template>
+<script>
+export default {
+  name: "Child1",
+  setup() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+div {
+  border: 1px solid green;
+}
+</style>
---
+++ Ch05/src/components/sub2/Child2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4>Child2</h4>
+  </div>
+</template>
+<script>
+export default {
+  name: "Child2",
+  setup() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+div {
+  border: 1px solid orange;
+}
+</style>
