329c895689496ac77b98c11b3ae77de78d0e28de
vhfl1 <111475086+vhfl1@users.noreply.github.com>
2023-05-15 13:00:53 +0900

    2023-05-15

---
+++ Ch08/src/components/sub2/Grids.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>반응형 레이아웃</h4>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column1</v-col>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column2</v-col>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column3</v-col>
+      <v-col cols="12" md="6" lg="3" class="border text-center">column4</v-col>
+    </v-row>
+  </v-container>
+</template>
+<script setup></script>
+<style scoped></style>
